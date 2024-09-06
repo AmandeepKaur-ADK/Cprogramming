e89076e6b28310993a90cbb75d50050b5c7ad815
Amandeep Kaur <107880259+AmandeepKaur-ADK@users.noreply.github.com>
2024-09-06 22:52:01 +0530

    Delete 6ascii.c

--- 6ascii.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    char value;
-     printf("Enter character: ");
-     scanf("%c", &value);
-    int ascii = (int)value;
-    printf("ASCII value is %d", ascii);
-    return 0;
-}
